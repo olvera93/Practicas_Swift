9a73ad6dd3db51b5dd695abbb9c1cf8c2b42c88e
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2021-12-14 11:59:45 -0800

    [ADD] Image
    Se agregò imagenes

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -2,17 +2,26 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Gonzalo on 14/12/2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI elements
+    @IBOutlet weak var diveImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diveImageView1?.image = #imageLiteral(resourceName: "DiceSix")
+        //diveImageView1.alpha = 0.5
+        
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+    
     }
 
 
