a0eae8403e102765110d45774df6864389f31837
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-06 11:58:27 -0600

    [ADD] BMI Calculator

---
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Gonzalo Olvera on 06/01/2022.
+//  Copyright © 2022 Gonzalo Olvera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
