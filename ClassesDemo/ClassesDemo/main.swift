70f5058947edfee9f1e5627f31b6103be2e22ae2
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-06 13:42:04 -0600

    [FINISH] ClassesDemo
    Se terminó este mini proyecto para saber como funciona las clases en Swift y diferenciar clases vs struct

--- ClassesDemo/ClassesDemo/main.swift
+++ ClassesDemo/ClassesDemo/main.swift
@@ -1,11 +1,20 @@
-//
-//  main.swift
-//  ClassesDemo
-//
-//  Created by Gonzalo Olvera Monroy on 06/01/22.
-//
 
-import Foundation
+var skeleton1 = Enemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton1
 
-print("Hello, World!")
+skeleton1.takeDamage(amount: 10)
 
+skeleton1.takeDamage(amount: 10)
+skeleton2.takeDamage(amount: 10)
+
+print(skeleton1.health)
+print(skeleton2.health)
+
+/*print("\n")
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane")
+dragon.move()
+dragon.attack()
+ */
