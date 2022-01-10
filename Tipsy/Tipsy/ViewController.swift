db2ee79b1b45a8a12f1936c649ccf17384af2fcc
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-10 11:26:10 -0600

    [ADD] Tipsy

---
+++ Tipsy/Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Gonzalo Olvera on 10/01/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
