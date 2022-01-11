8cf80a2d0ce4ecfddc9aab4058663fc12e207fea
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-11 12:12:13 -0600

    [FINISH] Protocols Demo
    Se llevó acabo de como funciona la palabra protocols en swift

--- Protocols Demo/Protocols Demo/main.swift
+++ Protocols Demo/Protocols Demo/main.swift
@@ -5,7 +5,66 @@
 //  Created by Gonzalo Olvera Monroy on 11/01/22.
 //
 
-import Foundation
+protocol CanFly {
+    func fly()
+}
 
-print("Hello, World!")
+
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+    
+    
+    
+}
+
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    
+    func soar() {
+        print("The eagle glides in the air using air currents.")
+    }
+    
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct FlyingMuseum {
+    
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+        
+    }
+    
+}
+
+struct Airplane: CanFly {
+    
+    func fly() {
+        print("The airplane use its engine to lift off into the air.")
+    }
+    
+}
+
+let myEagle = Eagle()
+let myPeguin = Penguin()
+let myPlane = Airplane()
+myEagle.fly()
+myPlane.fly()
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myEagle)
 
