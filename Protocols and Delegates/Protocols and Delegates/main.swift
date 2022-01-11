237996dbcb92c1a184143161e01d794039b7d35e
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-11 12:45:25 -0600

    [FINISH] Protocols and Delegates

--- Protocols and Delegates/Protocols and Delegates/main.swift
+++ Protocols and Delegates/Protocols and Delegates/main.swift
@@ -5,7 +5,66 @@
 //  Created by Gonzalo Olvera Monroy on 11/01/22.
 //
 
-import Foundation
 
-print("Hello, World!")
+protocol AdvancedLifeSupport {
+    
+    func performCPR()
+}
 
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+
+    func assessSituation() {
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+    
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The doctor does chest compression, 30 per second.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sounds.")
+    }
+    
+    
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings staying alive by the BeegGuees")
+    }
+    
+    func useElectricDrill(){
+        print("Whirr...")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let gonzalo = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
