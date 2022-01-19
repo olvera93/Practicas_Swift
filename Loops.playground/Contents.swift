c3c605ed99b888425842e492e7fae2409134cadb
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-19 12:43:34 -0600

    [ADD] Playground Lopps
    Es para saber como se manejar los ciclos en swift

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,6 @@
+
+let fruits = ["Apple", "Pear", "Orange"]
+let contacts = ["Adam": 123456789, "James":987654321, "Amy": 912837365]
+let word = "supercalifragilisticexpialidociuos"
+let halfOpenRange = 1..<5
+let closedRange = 1...5
