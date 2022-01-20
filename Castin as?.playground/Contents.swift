622d3904935d95990a654e6c0857167ed164bcde
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-20 13:52:46 -0600

    [FINISH] Casting

--- Castin as?.playground/Contents.swift
+++ Castin as?.playground/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 class Animal {
     var name: String
     
-    init(n: String) {
+    init(_ n: String) {
         name = n
     }
 }
@@ -16,10 +16,37 @@ class Human: Animal {
     
 }
 
-class Fish: Animal {
+struct Fish {
     
     func breathUnderWater() {
         print("Breathing under water.")
     }
     
 }
+
+let gonzalo = Human("Gonzalo")
+let jack = Human("Jack")
+let nemo = Fish()
+let num: NSNumber = 12
+let word: NSString = "abc"
+
+let neighbours: [NSObject] = [num, word]
+
+func findNemo(from animals:  [Animal]) {
+    for animal in animals {
+        if animal is Fish {
+            print(animal.name)
+            let fish = animal as! Fish
+            fish.breathUnderWater()
+        }
+    }
+}
+
+
+if let fish = neighbours[1] as? Fish {
+    fish.breathUnderWater()
+} else {
+    print("Casting Fail")
+}
+
+
