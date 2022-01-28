18a70825c6283139f6de1421ce8bec5807af60dc
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-28 17:00:24 -0600

    [ADD] I Am RichSwiftUI
    Para saber como funciona SwiftUI

---
+++ I Am RichSwiftUI/I Am RichSwiftUI/I_Am_RichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichSwiftUIApp.swift
+//  I Am RichSwiftUI
+//
+//  Created by Gonzalo Olvera Monroy on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
