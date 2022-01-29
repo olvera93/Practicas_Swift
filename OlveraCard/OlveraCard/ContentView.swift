5501b8fc3a68b9f1c958b9067088960fd1c4e30c
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-28 18:13:33 -0600

    [FINISH] OlveraCard

--- OlveraCard/OlveraCard/ContentView.swift
+++ OlveraCard/OlveraCard/ContentView.swift
@@ -11,11 +11,38 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Color(.green)
+            Color(red: 0.09, green: 0.63, blue: 0.52)
                 .edgesIgnoringSafeArea(.all)
             
-            Text("Hello, world!")
-                .padding()
+            VStack {
+                
+                Image("apple")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Gonzalo Olvera")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                Divider()
+                
+                InfoView(text: "52+ 55 7488 8899", imageName: "phone.fill")
+                InfoView(text: "olvera9302@outlook.com", imageName: "envelope.fill")
+                
+                
+                
+                
+            }
+                
         }
         
         
@@ -28,3 +55,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ OlveraCard/OlveraCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  OlveraCard
+//
+//  Created by Gonzalo Olvera Monroy on 28/01/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+        
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
