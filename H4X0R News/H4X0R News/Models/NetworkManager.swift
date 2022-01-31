a365360c8409305f01b9d25ac16e612d8556aa38
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-31 13:33:39 -0600

    [FINISH] H4X0R News

---
+++ H4X0R News/H4X0R News/Models/NetworkManager.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkManager.swift
+//  H4X0R News
+//
+//  Created by Gonzalo Olvera Monroy on 31/01/22.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+   @Published var posts = [Post]()
+    
+    func fetchData() {
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let sesseion = URLSession(configuration: .default)
+            let task = sesseion.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+
+                        } catch {
+                            print(error)
+                        }
+                        
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
---
+++ H4X0R News/H4X0R News/Views/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Gonzalo Olvera Monroy on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        
+        NavigationView {
+            List(networkManager.posts) { post in
+                
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }
+            .navigationTitle("H4X04 NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
