009735a7e9bf0c152108cc8c30adac5266911069
Gonzalo Olvera Monroy <42697554+olvera93@users.noreply.github.com>
2022-01-31 11:55:47 -0600

    [ADD] Dice-SwiftUI
    La misma aplicación de dados pero ahora con SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Gonzalo Olvera Monroy on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
