837ca2ac16e2d67aaf6a62813b31aca284094bb6
Gonzalo Olvera <42697554+olvera93@users.noreply.github.com>
2022-06-07 18:45:10 -0500

    [ADD] ScrumdingerApp

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Gonzalo Olvera Monroy on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
